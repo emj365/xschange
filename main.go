196aed6254edf8dbc51156174458d23ba06c2875
Michael JIN <emj365@gmail.com>
2018-12-12 13:10:06 +0800

    basic structure

--- main.go
+++ main.go
@@ -1,17 +1,88 @@
 package main
 
-type User struct {
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type orderType bool
+type orderStatus string
+
+const (
+	selling orderType   = false
+	buying  orderType   = true
+	open    orderStatus = "OPEN"
+	closed  orderStatus = "CLOSE"
+)
+
+var sellings []*order
+var buyings []*order
+var orders []*order
+
+// User data
+type user struct {
 	ID      uint
-	balance int
+	Balance int
 }
 
-type Order struct {
+// Order created by the user
+type order struct {
+	UserID   uint
+	Type     orderType
 	Quantity int
 	Price    int
-	UserID   uint
-	Sell     bool
+	Matchs   []*match
+	Status   orderStatus
 }
 
-var sellingOrders []Order
-var buyingOrders []Order
-var orders []Order
+// Match is one of trades of order
+type match struct {
+	Order    *order
+	Quantity int
+	Price    int
+}
+
+func (o *order) doSettlement() {
+}
+
+func (o *order) match() bool {
+	return true
+}
+
+func (o *order) place() {
+}
+
+func (o *order) create() {
+	orders = append(orders, o)
+	if remain := o.match(); remain {
+		o.place()
+	} else {
+		o.Status = closed
+	}
+
+	if len(o.Matchs) > 0 {
+		o.doSettlement()
+	}
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/orders", getOrders).Methods("GET")
+	router.HandleFunc("/orders", postOrders).Methods("POST")
+}
+
+func extractOrderFromRequest(r *http.Request, o *order) {
+	json.NewDecoder(r.Body).Decode(o)
+}
+
+func getOrders(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(orders)
+}
+
+func postOrders(w http.ResponseWriter, r *http.Request) {
+	var o order
+	extractOrderFromRequest(r, &o)
+	o.create()
+}
